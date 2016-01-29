7d58f397e63ee92bccecc84830aafb9cd221e9c3
WANG Guoqin <wangguoqin1001@gmail.com>
2016-01-30 05:35:42 +0800

    use schema:load instead of migrate

--- script/production_upgrade_server.sh
+++ script/production_upgrade_server.sh
@@ -6,7 +6,7 @@ RAILS_ENV=production
 export RAILS_ENV
 
 git pull
-rake db:migrate
+rake db:schema:load
 #rake db:data:load_dir dir="datadump"
 rake assets:clean
 rake assets:precompile
--- script/testing_upgrade_server.sh
+++ script/testing_upgrade_server.sh
@@ -3,5 +3,5 @@
 set -ex
 
 git pull
-rake db:migrate
+rake db:schema:load
 #rake db:data:load_dir dir="datadump"
