410b29472ce324490823df0c7816f1922c2aaaab
lmiogit <yl_mio@sina.com>
2016-02-16 14:18:07 +0800

    slider-modules

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,47 +1,6 @@
 $(function () {
         var action = function () {
-            var all = $('.sliders').find('[data-src]');
-            var listLength = all.length;
-            $.each(all, function (k, v) {
-                var _self = $(v);
-                _self.css({"background-image": "url(" + _self.attr('data-src') + ")"});
-            });
-            $(all[0]).show();
-            $(all[1]).hide();
-            $(all[2]).hide();
-            var s = $('.switch');
-            var a = s.find('.article');
-            if (window.innerWidth > 1200) {
-                s.css({'left': (window.innerWidth - 150) / 2});
-            } else {
-                s.css({'left': '525px'})
-            }
-            a.css({'width': 1 / (listLength) * 100 + '%'});
-
-            s.on('click', function (event) {
-                event.preventDefault();
-                console.log(0);
-                var x = event.pageX;
-                var y = $(this).offset().left;
-                var z = x - y;
-                if (z <= 50) {
-                    a.css({"margin-left": "0"});
-                    $(all[0]).fadeIn();
-                    $(all[1]).fadeOut();
-                    $(all[2]).fadeOut();
-                } else if (z <= 100) {
-                    a.css({"margin-left": "50px"});
-                    $(all[1]).fadeIn();
-                    $(all[0]).fadeOut();
-                    $(all[2]).fadeOut();
-                } else {
-                    a.css({"margin-left": "100px"});
-                    $(all[2]).fadeIn();
-                    $(all[1]).fadeOut();
-                    $(all[0]).fadeOut();
-                }
-            });
-            //slider.init();
+            slider.init('.slider-item', 5000);
             $('.list').find('.item').on('click', function () {
                 var index = $(this).index();
                 $(this).find('.item-title').addClass('active');
@@ -52,23 +11,28 @@ $(function () {
         };
 
         var slider = {
-            init: function () {
-                var items = $('.sliders').find('[data-src]');
+            init: function (selector, interval) {
+                var items = $(selector);
                 $.each(items, function (k, v) {
                     var _self = $(v);
-                    _self.css({"background-image": "url(" + _self.attr('data-src') + ")"});
+                    _self.css({"background-image": "url(" + _self.attr('data-src') + ")"});//lazy load
                     if (k === 0) {
                         _self.show();
                     } else {
                         _self.hide();
                     }
                 });
-                slider.option.length = items.length;
-                slider.option.currentIndex = 0;
-
                 var s = $('.switch');
                 var a = s.find('.article');
-                a.css({'width': 1 / (slider.option.length) * 100 + '%'});
+                a.css({'width': 1 / (items.length) * 100 + '%'});
+                var step = (1 / items.length) * 150;
+
+                slider.option.selector = selector;
+                slider.option.length = items.length;
+                slider.option.currentIndex = 0;
+                slider.option.article = a;
+                slider.option.step = step;
+
                 s.on('click', function (event) {
                     event.preventDefault();
                     var x = event.pageX;
@@ -76,15 +40,38 @@ $(function () {
                     var z = x - y;
 
                     for (var i = 0; i < slider.option.length; i++) {
-                        if (z <= i * (1 / slider.option.length)) {
+                        if (z <= (i + 1) * slider.option.step) {
                             slider.changeByIndex(i);
+                            break;
                         }
                     }
-                })
+
+                });
+
+                if (arguments.length >= 2 && typeof arguments[1]) {
+                    console.log('auto');
+                    window.setInterval(function () {
+                        var cI = slider.option.currentIndex;
+                        var l = slider.option.length;
+                        var nI = cI + 1;
+                        if (nI === l) {
+                            nI = 0;
+                        }
+                        slider.changeByIndex(nI);
+                    }, interval)
+                }
             },
             option: {},
             changeByIndex: function (index) {
-
+                if (slider.option.currentIndex === index) {
+                    return index;
+                } else {
+                    slider.option.currentIndex = index;
+                    slider.option.article.css({'margin-left': index * slider.option.step});
+                    var items = $(slider.option.selector);
+                    items.eq(index).fadeIn();
+                    items.eq(index).siblings().fadeOut();
+                }
             }
         };
 
