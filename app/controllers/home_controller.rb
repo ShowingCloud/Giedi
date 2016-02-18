6574c28884ec479e9b493e09008a5ea6aac76b9e
huaxiukun <huaxiukun@yiyc.com>
2016-02-18 17:14:34 +0800

    404

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def error_404
+    render_404
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'activities/index'
   get 'services/index'
-
+  match '*path', via: :all, to: 'home#error_404'
 end
