6ede7f839574258aac0a6aac19e89c344db30b78
huaxiukun <huaxiukun@yiyc.com>
2016-09-08 13:42:54 +0800

    remove js to footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions], controllers: { cas_sessions: 'our_cas_sessions' }
-  devise_scope :user do
-    get "sign_in", to: "devise/cas_sessions#new"
-    delete "sign_out", to: "devise/cas_sessions#destroy"
-  end
   root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:sessions], controllers: {cas_sessions: 'our_cas_sessions'}
+  devise_scope :user do
+    get "sign_in", to: "auth/cas_sessions#new"
+    delete "sign_out", to: "auth/cas_sessions#destroy"
+  end
 end
