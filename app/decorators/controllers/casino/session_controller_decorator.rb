8087fe6d57e791c7c2eca0fd7aa13c3a98621040
WHT <wht@WHTdeMacBook-Pro.local>
2016-09-12 14:18:23 +0800

    need email confirmation

--- app/decorators/controllers/casino/session_controller_decorator.rb
+++ app/decorators/controllers/casino/session_controller_decorator.rb
@@ -10,4 +10,22 @@ CASino::SessionsController.class_eval do
     end
   end
 
+  def create
+    validation_result = validate_login_credentials(params[:username], params[:password])
+    if !validation_result
+      log_failed_login params[:username]
+      show_login_error I18n.t('login_credential_acceptor.invalid_login_credentials')
+    else
+      extra_attributes = validation_result[:user_data][:extra_attributes]
+      if extra_attributes[:confirmed] || extra_attributes[:mobile].present?
+        sign_in(validation_result, long_term: params[:rememberMe], credentials_supplied: true)
+      else
+        @user_name = extra_attributes[:name]
+        @email = extra_attributes[:email]
+        session[:user_id]=extra_attributes[:guid]
+        render "users/before_confirmed"
+      end
+    end
+  end
+
 end
