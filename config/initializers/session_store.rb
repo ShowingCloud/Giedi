22fc2470da07edde387a798e8fb5412d49c8a056
WHT <wht@WHTdeMacBook-Pro.local>
2016-09-26 14:10:15 +0800

    use readthis session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_dome-sso_session', expire_after: 5.minutes
+Rails.application.config.session_store :cache_store, expire_after: 10.minutes.to_i
