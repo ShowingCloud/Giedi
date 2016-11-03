6ef331e6b1c58605d74d2a889aede2ad461e8364
WHT <wht@WHTdeMacBook-Pro.local>
2016-11-03 11:50:43 +0800

    fix phone validatation

--- app/controllers/phone_verifications_controller.rb
+++ app/controllers/phone_verifications_controller.rb
@@ -8,10 +8,10 @@ class PhoneVerificationsController < ApplicationController
   def create
     if verify_rucaptcha?
       phone = Phonelib.parse(params[:phone])
-      render(json: { msg: 'invalid phone number', code: 'E001' }, status: 422) && return unless phone.valid? && phone.type == :mobile && params[:phone] == 11
+      render(json: { msg: 'invalid phone number', code: 'E001' }, status: 422) && return unless phone.valid? && phone.type == :mobile && params[:phone].length == 11
       render(json: { msg: 'phone existed', code: 'E002' }, status: 422) && return if User.find_by(phone: params[:phone]).present?
       @phone_verification = Rails.cache.read(params[:phone])
-      render json: { msg: 'sms too often', code: 'E005' }, status: 403 && return if @phone_verification.present? && @phone_verification[:send_at] < 20.seconds.ago
+      render(json: { msg: 'sms too often', code: 'E005' }, status: 403) && return if @phone_verification.present? && @phone_verification[:send_at] < 20.seconds.ago
       SendSmsJob.perform_later(params[:phone])
       render json: { msg: 'sms send' }, status: 200
     else
