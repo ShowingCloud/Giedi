07f7b899121c90838cb225e74bf98fb66d46b88e
WHT <wht@WHTdeMacBook-Pro.local>
2016-12-22 17:52:41 +0800

    upgrade mina to 1.0 version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,38 @@
-require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/bundler'
+require 'mina/rvm'
 require 'mina/puma'
 require 'mina_sidekiq/tasks'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (https://rvm.io)
+
 # Basic settings:
 #   domain       - The hostname to SSH to.
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
+set :application_name, 'dome-sso'
 set :domain, 'wht@dev.domelab.com'
 set :deploy_to, '/home/wht/work/dome-sso'
 set :repository, 'git@github.com:Bikeman18/dome-sso.git'
 set :branch, 'master'
 
-# For system-wide RVM install.
-set :rvm_path, '/usr/local/rvm/scripts/rvm'
-set :identity_file, 'config/deploy_id_rsa' if File.exist?('config/deploy_id_rsa')
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/settings/production.local.yml','log','tmp/sockets', 'tmp/pids']
-set :puma_config, -> { "#{deploy_to}/#{current_path}/config/puma_app.rb" }
+set :rvm_use_path, '/usr/local/rvm/scripts/rvm'
+set :shared_files, fetch(:shared_files, []).push('config/settings/production.local.yml','log','tmp/sockets', 'tmp/pids')
+set :puma_config, -> { "#{:deploy_to}/#{:current_path}/config/puma_app.rb" }
+
 # Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
+#   set :user, 'foobar'          # Username in the server to SSH to.
+#   set :port, '30000'           # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
-# This task is the environment that is loaded for most commands, such as
+# shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
@@ -36,48 +40,35 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
-  invoke :'rvm:use[ruby-2.3.1@default]'
+  invoke :'rvm:use', 'ruby-2.3.3'
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
-task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/shared/pids/"]
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  # command %{rbenv install 2.3.0}
+  command %(mkdir -p "#{fetch(:deploy_to)}/shared/pids/")
+  command %(mkdir -p "#{fetch(:deploy_to)}/shared/log")
+  command %(chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/log")
 
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config/settings"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config/settings"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/settings/production.local.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/settings/production.local.yml'."]
+  command %(mkdir -p "#{fetch(:deploy_to)}/shared/config")
+  command %(chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/config")
+  command %(mkdir -p "#{fetch(:deploy_to)}/shared/config/settings")
+  command %(chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/config/settings")
+  command %(touch "#{fetch(:deploy_to)}/shared/config/settings/production.local.yml")
+  command  %(echo "-----> Be sure to edit '#{fetch(:deploy_to)}/shared/config/settings/production.local.yml'.")
 
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/sockets"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/sockets"]
+  command %(mkdir -p "#{fetch(:deploy_to)}/shared/tmp/sockets")
+  command %(chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/tmp/sockets")
 
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/pids"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/pids"]
-
-  if repository
-    repo_host = repository.split(%r{@|://}).last.split(%r{:|\/}).first
-    repo_port = /:([0-9]+)/.match(repository) && /:([0-9]+)/.match(repository)[1] || '22'
-
-    queue %[
-      if ! ssh-keygen -H  -F #{repo_host} &>/dev/null; then
-        ssh-keyscan -t rsa -p #{repo_port} -H #{repo_host} >> ~/.ssh/known_hosts
-      fi
-    ]
-  end
+  command %(mkdir -p "#{fetch(:deploy_to)}/shared/tmp/pids")
+  command %(chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/tmp/pids")
 end
 
 desc "Deploys the current version to the server."
-task :deploy => :environment do
-  to :before_hook do
-    # Put things to run locally before ssh
-  end
+task :deploy do
+  # uncomment this line to make sure you pushed your local branch to the remote origin
+  # invoke :'git:ensure_pushed'
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
@@ -89,16 +80,21 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
-    to :launch do
+    on :launch do
+      invoke :'rvm:use', 'ruby-2.3.3'
       invoke :'puma:restart'
       invoke :'sidekiq:restart'
+      # in_path(fetch(:current_path)) do
+      #   command %{mkdir -p tmp/}
+      #   command %{touch tmp/restart.txt}
+      # end
     end
   end
+
+  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+  # run(:local){ say 'done' }
 end
 
 # For help in making your deploy script, see the Mina documentation:
 #
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
+#  - https://github.com/mina-deploy/mina/tree/master/docs
