472dc9351f94ac86c391c00166d3d90942e62840
www-data @ SCS.IM <www-data@scs.im>
2019-08-24 18:39:59 +0000

    Updated startup scripts and URLs

--- script/production_start_server.sh
+++ script/production_start_server.sh
@@ -3,6 +3,5 @@
 set -x
 
 sidekiqctl stop tmp/pids/sidekiq.pid 60
-thin -p 3290 -e production stop
-thin -d -p 3290 -e production --tag "SSO V1" start
+pumactl restart
 sidekiq -C config/sidekiq.yml -d -e production --tag "SSO V1"
