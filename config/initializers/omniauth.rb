c74c1d506b4f26929f8d8f99cae50a3da45dced2
www-data @ SCS.IM <www-data@scs.im>
2019-08-24 18:39:03 +0000

    Sensitive configuration files were removed (repo not tainted), while some others updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.logger = Rails.logger
   provider :github, Settings.github.key, Settings.github.secret
-  provider :weibo, Settings.weibo.key, Settings.weibo.secret,token_params: {redirect_uri: "http://account.domelab.com/auth/weibo/callback" }, :image_size => 'original'
+  provider :weibo, Settings.weibo.key, Settings.weibo.secret,token_params: {redirect_uri: "http://domelab-sso.scs.im/auth/weibo/callback" }, :image_size => 'original'
   provider :qq_connect, Settings.qq.key, Settings.qq.secret
-  provider :wechat, Settings.wechat.key, Settings.wechat.secret,:authorize_params => {:redirect_uri => "http://account.domelab.com/auth/wechat/callback"}
+  provider :wechat, Settings.wechat.key, Settings.wechat.secret,:authorize_params => {:redirect_uri => "http://domelab-sso.scs.im/auth/wechat/callback"}
 end
---
+++ config/puma.rb
@@ -0,0 +1,12 @@
+threads 0,16
+workers 3
+preload_app!
+
+environment 'production'
+daemonize
+pidfile '/var/www/rails/dome/sso/v1/tmp/pids/puma.pid'
+state_path '/var/www/rails/dome/sso/v1/tmp/pids/puma.state'
+stdout_redirect '/var/www/rails/dome/sso/v1/log/stdout', '/var/www/rails/dome/sso/v1/log/stderr', true
+
+bind 'tcp://0.0.0.0:3290'
+tag 'SSO v1'
